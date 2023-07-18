745be49bf82ef335ac7f66f4bea42629e0dcadbb
shedrack-prog <usheddy07@gmail.com>
2023-07-18 21:54:37 +0100

    fixing errors

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,10 +8,10 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboards('21c1facf-bd2e-4763-893b-ae18af5ee695');
+  const billboard = await getBillboards('04f8545a-fd1f-4a2b-9496-2f97a7b20dab');
   return (
     <Container>
-      <div className="space-y-10 pb-10 ">
+      <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
         <div className="flex flex-col  gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
